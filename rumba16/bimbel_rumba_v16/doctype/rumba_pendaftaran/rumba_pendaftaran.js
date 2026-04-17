d4ac9cad76f425cb3717cc5e8221915814321d93
Rumba Kita <yrki@vmi2685747.contaboserver.net>
2026-04-17 11:58:04 +0700

    feat(Doctype): Create DocType Rumba Pendaftaran

---
+++ rumba16/bimbel_rumba_v16/doctype/rumba_pendaftaran/rumba_pendaftaran.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Yayasan Rumba Kita Indonesia and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Rumba Pendaftaran", {
+// 	refresh(frm) {
+
+// 	},
+// });
