21d96bd27d2c8e861d9004861e2e03488dc7e7ca
Rumba Kita <yrki@vmi2685747.contaboserver.net>
2026-05-26 11:02:09 +0700

    update doctype rumba murid dan pendaftaran from local

--- rumba16/bimbel_rumba_v16/doctype/rumba_pendaftaran/rumba_pendaftaran.py
+++ rumba16/bimbel_rumba_v16/doctype/rumba_pendaftaran/rumba_pendaftaran.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2026, Yayasan Rumba Kita Indonesia and contributors
 # For license information, please see license.txt
 
+import re
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
