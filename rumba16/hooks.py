ebff4274938e9661c8c630cc40d1f633d3a27542
Rumba Kita <yrki@vmi3126847.contaboserver.net>
2026-07-10 13:45:43 +0700

    Update css.

--- rumba16/hooks.py
+++ rumba16/hooks.py
@@ -24,11 +24,11 @@ add_to_apps_screen = [
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/rumba16/css/rumba16.css"
+app_include_css = "/assets/rumba16/css/rumba_theme.css"
 # app_include_js = "/assets/rumba16/js/rumba16.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/rumba16/css/rumba16.css"
+web_include_css = "/assets/rumba16/css/rumba_web.css"
 # web_include_js = "/assets/rumba16/js/rumba16.js"
 
 # include custom scss in every website theme (without file extension ".scss")
