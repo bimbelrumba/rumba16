69b47db9648c0e38158a39c2271066c3779af3dd
Rumba Kita <yrki@vmi3126847.contaboserver.net>
2026-07-18 13:52:41 +0700

    fixtures: update Kemitraan dan Pemasaran.

--- rumba16/hooks.py
+++ rumba16/hooks.py
@@ -346,7 +346,7 @@ fixtures = [
     {"dt": "Custom HTML Block", "filters": [["name", "in", [
     "Distribusi Murid per Program (Donut)", "Murid Sering Absen (Alpa)",
     "Sesi Saya Hari Ini", "Sumber Informasi Lead",
-    "Tutor Tanpa Kelas (Pengampu)",
+    "Tutor Tanpa Kelas (Pengampu)", "Papan Event Aktif",
     ]]]},
     {
     "dt": "Custom Role",
