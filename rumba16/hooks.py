fd54fac272e4361e85e46fe668f5b98cac7aa034
Rumba Kita <yrki@vmi3126847.contaboserver.net>
2026-07-21 20:36:40 +0700

    fixtures: contract naming.

--- rumba16/hooks.py
+++ rumba16/hooks.py
@@ -26,7 +26,9 @@ website_context = {
 }
 
 app_logo_url = "/assets/rumba16/images/splash-singa.png"
-
+<<<<<<< HEAD
+=======
+>>>>>>> 2de97a2 (fixtures: contract naming.)
 # Includes in <head>
 # ------------------
 
